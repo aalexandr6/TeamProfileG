5420ab308a8535fb97a559c634665cd681c19748
Aaron Padilla <codewizard@Aarons-MacBook-Air.local>
2023-05-12 21:42:52 -0400

    index.js will be the entry point

---
+++ index.js
@@ -0,0 +1,15 @@
+//Index is the directory and will be the entry point for the application
+const fs = require ('fs')
+const inquirer = require ('inquirer')
+
+//importing classes for the application
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require('./lib/Employee')
+
+
+
+
+
+
